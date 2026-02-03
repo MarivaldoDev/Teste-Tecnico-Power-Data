62c1d000b31498c6369b34d936d849678faa0fc5
Marivaldo Pedro <pedromarivaldo10@gmail.com>
2026-02-03 13:56:53 -0300

    style: criando template para a página inicial

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from app.routes import people, films, planets, starships
 
 app = FastAPI(
@@ -7,12 +9,17 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 app.include_router(people.router, prefix="/people", tags=["People"])
 app.include_router(planets.router, prefix="/planets", tags=["Planets"])
 app.include_router(films.router, prefix="/films", tags=["Films"])
 app.include_router(starships.router, prefix="/starships", tags=["Starships"])
 
 
+templates = Jinja2Templates(directory="app/templates")
+
+
 @app.get("/")
-def health_check():
-    return {"status": "ok"}
+def health_check(request: Request):
+    return templates.TemplateResponse("health.html", {"request": request})
